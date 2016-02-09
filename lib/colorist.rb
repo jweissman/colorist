ac6581b33aea03633f7889d43ad3d1099c0e1e3d
Joseph Weissman <joe@bignerdranch.com>
2016-02-08 20:03:14 -0500

    mix colors

--- lib/colorist.rb
+++ lib/colorist.rb
@@ -1,1 +1,75 @@
 require 'colorist/version'
+
+class Color < Struct.new(:name,:red,:green,:blue); end
+
+Red   = Color.new(:red,   256,   0,   0)
+Green = Color.new(:green,   0, 256,   0)
+Blue  = Color.new(:blue,    0,   0, 256)
+
+Orange = Color.new(:orange, 256, 165, 0)
+Yellow = Color.new(:yellow, 256, 256, 0)
+
+White = Color.new(:white, 256, 256, 256)
+Purple = Color.new(:purple, 128, 0, 128)
+Lavender = Color.new(:lavender, 192, 128, 192)
+
+Black = Color.new(:black, 0, 0, 0)
+
+SimplePalette = [ Red, Green, Blue, Orange, Yellow, White, Black ]
+
+Peach = Color.new(:peach, 256, 218, 185)
+Sienna = Color.new(:sienna, 160,82,45)
+
+class Mixture
+  attr_reader :sources
+
+  def initialize((a,b))
+    @sources = [a,b]
+  end
+
+  def color
+    color_result = Color.new(
+      sources.map(&:name).join('-'),
+      average(sources.map(&:red)),
+      average(sources.map(&:green)),
+      average(sources.map(&:blue))
+    )
+    color_result
+  end
+
+  MAX_DEPTH = 4
+  ALLOWANCE = 5
+  def self.infer(target_color, palette=SimplePalette, depth=0)
+    raise "Could not mix #{target_color} (mix depth exceeded)" if depth > MAX_DEPTH
+
+    # p [ :infer, target: target_color, palette: palette, depth: depth ]
+
+    first_to_mix,second_to_mix = palette.combination(2).detect do |combo| #(a,b)|
+      print '.'
+      mixture = Mixture.new(combo) #[a,b])
+
+      d_red, d_blue, d_green = 
+        (mixture.color.red - target_color.red).abs, 
+        (mixture.color.blue - target_color.blue).abs, 
+        (mixture.color.green - target_color.green).abs
+
+      d_red < ALLOWANCE && d_blue < ALLOWANCE && d_green < ALLOWANCE
+    end
+
+    #aise "Couldn't mix #{target_color}"
+    if first_to_mix.nil? || second_to_mix.nil?
+      palette += palette.combination(2).map do |combo|
+        Mixture.new(combo).color
+      end
+
+      infer(target_color, palette, depth+1)
+    else
+      Mixture.new([first_to_mix, second_to_mix])
+    end
+  end
+
+  protected
+  def average(as)
+    (as.inject(&:+) / as.size)
+  end
+end
--- spec/colorist_spec.rb
+++ spec/colorist_spec.rb
@@ -1,8 +1,100 @@
+require 'pry'
 require 'spec_helper'
 require 'colorist'
 
-describe Colorist do
-  it "should have a VERSION constant" do
-    expect(subject.const_get('VERSION')).to_not be_empty
+describe Color do
+  subject(:color) { Color.new(:white,255,255,255) }
+
+  it 'should have a name' do
+    expect(color.name).to eq(:white)
+  end
+
+  it 'should have values for each channel' do
+    expect(color.red).to eq(255)
+    expect(color.blue).to eq(255)
+    expect(color.green).to eq(255)
+  end
+
+  context 'should have some named colors' do
+    it 'should have red' do
+      expect(Red.name).to eq(:red)
+      expect(Red.red).to eq(256)
+      expect(Red.blue).to eq(0)
+      expect(Red.green).to eq(0)
+    end
+
+    it 'should have green' do
+      expect(Green.name).to eq(:green)
+      expect(Green.red).to eq(0)
+      expect(Green.green).to eq(256)
+      expect(Green.blue).to eq(0)
+    end
+
+    it 'should have blue' do
+      expect(Blue.name).to eq(:blue)
+      expect(Blue.blue).to eq(256)
+      expect(Blue.red).to eq(0)
+      expect(Blue.green).to eq(0)
+    end
+  end
+end
+
+describe Mixture do
+  subject(:mixture) do
+    Mixture.new([Red,Blue])
+  end
+
+  let(:mixed_color) { mixture.color }
+
+  it 'should be a color' do
+    expect(mixed_color).to be_a(Color)
+  end
+
+  it 'should have averaged RGB values' do
+    expect(mixed_color.red).to eq(128)
+    expect(mixed_color.blue).to eq(128)
+  end
+
+  describe '.infer' do
+    context 'with a simple combination' do
+      subject(:mixture) do
+        Mixture.infer(Purple)
+      end
+
+      it 'should have source colors Red and Blue' do
+        expect(mixture.sources).to eq([Red, Blue])
+      end
+    end
+
+    context 'with a two-step combination' do
+      subject(:mixture) do
+        Mixture.infer(Lavender, SimplePalette)
+      end
+
+      it 'should have source colors Red and Blue' do
+        expect(mixture.sources.map(&:name)).to eq([:white, "red-blue"])
+      end
+    end
+
+    context 'can mix Peach' do
+      subject(:mixture) do
+        Mixture.infer(Peach, SimplePalette)
+      end
+
+      it 'should have source colors white and white-red-orange' do
+        expect(mixture.sources.map(&:name)).to eq([:white, "white-red-orange"])
+      end
+    end
+
+    context 'can mix Sienna' do
+      subject(:mixture) do
+        Mixture.infer(Sienna, SimplePalette)
+      end
+
+      it 'should have source colors red and green-blue-red-white' do
+        binding.pry
+        expect(mixture.sources.map(&:name)).to eq([:red, "green-blue-red-white"])
+      end
+    end
   end
 end
